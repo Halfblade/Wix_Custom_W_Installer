e903c18f4199630ef0e5d7d79c1ff17ac6ac94a0
Jason Moore <leahmjason@gmail.com>
2022-04-22 18:13:49 -0400

    Created .NetCore app to run the installer

---
+++ Wix_Custom_W_Installer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wix_Custom_W_Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
